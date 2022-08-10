a378a5867f365de944bc1e36ee8a4d5da89585f0
Nakade Shoya <doshouya@gmail.com>
2022-08-10 18:47:08 +0900

    Initial Commit

---
+++ ReceptionForLinkup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReceptionForLinkup
+//
+//  Created by 中出翔也 on 2022/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReceptionForLinkup/ReceptionForLinkupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceptionForLinkupApp.swift
+//  ReceptionForLinkup
+//
+//  Created by 中出翔也 on 2022/08/10.
+//
+
+import SwiftUI
+
+@main
+struct ReceptionForLinkupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
